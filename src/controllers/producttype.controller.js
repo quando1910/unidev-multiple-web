887c01086a1b569a03ca920b41b4ee1a550dcd62
quan do <quan.do@asiantech.vn>
2019-02-04 18:56:06 +0700

    1234

--- src/controllers/producttype.controller.js
+++ src/controllers/producttype.controller.js
@@ -5,7 +5,7 @@ let { actions, asyncMiddleware, model } = constructor('Producttype')
  * Adding new action here
  */
 
-actions.new = asyncMiddleware(async (req, res, next) => {
+actions.index = asyncMiddleware(async (req, res, next) => {
   return await model.find()
 })
 
